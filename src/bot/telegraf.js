bb2e397ac3fdf458a56ff0d3c87fb905712ddbb2
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-08-12 11:20:27 -0300

    feat: add try catch on telegranf function

--- src/bot/telegraf.js
+++ src/bot/telegraf.js
@@ -11,31 +11,36 @@ const {
 } = require('./commands');
 
 const initTelegraf = async () => {
+  try {
 
-  configureScenens(bot);
-
-  bot.use(hasPermission);
-  bot.command('start', commandList);
-  bot.command('criador', about);
-  bot.command('urlgoogle', saveUrlSheWizardScenes);
-  bot.command('definirgrupo', saveGroupIdDefault);
-  bot.command('comandos', commandList);
-  bot.command('rotinas', routineInfo);
-  bot.command('ajuda', commandList);
-  bot.command('informacoesdiaria', (context) => dailyInfo(context, true));
-  
-  console.log('Bot RUNING...');
-  
-  await bot.telegram.deleteWebhook();
-
-  if(process.env?.URL_CLOUD.trim()) {
-    await bot.telegram.setWebhook(process.env.URL_CLOUD + '/apibot' || '')
+    configureScenens(bot);
+    
+    bot.use(hasPermission);
+    bot.command('start', commandList);
+    bot.command('criador', about);
+    bot.command('urlgoogle', saveUrlSheWizardScenes);
+    bot.command('definirgrupo', saveGroupIdDefault);
+    bot.command('comandos', commandList);
+    bot.command('rotinas', routineInfo);
+    bot.command('ajuda', commandList);
+    bot.command('informacoesdiaria', (context) => dailyInfo(context, true));
+    
+    console.log('Bot RUNING...');
+    
+    await bot.telegram.deleteWebhook();
+    
+    if(process.env?.URL_CLOUD.trim()) {
+      await bot.telegram.setWebhook(process.env.URL_CLOUD + '/apibot' || '')
+    }
+    
+    const botInfo = (await bot.telegram.getWebhookInfo());
+    console.log('Bot INFO:', botInfo);
+    
+    await bot.launch();
+    
+  } catch(error) {
+    console.log('Ocorreu um erro no initTelegraf:', error);
   }
-  
-  const botInfo = (await bot.telegram.getWebhookInfo());
-  console.log('Bot INFO:', botInfo);
-
-  await bot.launch();
 
 }
 
